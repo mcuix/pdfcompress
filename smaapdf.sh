66452ca3f0599a3b29f9283777a13cd6af07931b
mcuix <mcuix@users.noreply.github.com>
2017-11-22 17:29:44 +0100

    Update smaapdf.sh

--- smaapdf.sh
+++ smaapdf.sh
@@ -4,7 +4,7 @@ read -p "Enter compression level (screen, ebook, printer, prepress)  : " level
 read -p "Enter name of output PDF : " outputpdf
 read -p "Enter name of input PDF : " inputpdf
 
-# display back 3 numbers - punched by user.
+# Start compression
 gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/$level -dNOPAUSE -dQUIET -dBATCH -sOutputFile=$outputpdf $inputpdf
 
 echo "The compressed pdf has been saved as - $outputpdf"
